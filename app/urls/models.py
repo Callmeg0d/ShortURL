3fcf5494b7d25b84a5ba7120769995e44f40d963
Egor Muratov <egor.muratov.27@mail.ru>
2025-06-03 16:15:05 +0300

    feat: added pagination and filtering, detailed statistics on the links

--- app/urls/models.py
+++ app/urls/models.py
@@ -11,19 +11,29 @@ class URL(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     original_url: Mapped[str] = mapped_column(String, nullable=False)
-    short_url: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    short_url: Mapped[str] = mapped_column(
+        String, unique=True, nullable=False, index=True
+    )
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
     clicks: Mapped[int] = mapped_column(Integer, default=0)
 
-    clicks_logs: Mapped[list["ClickLog"]] = relationship("ClickLog", back_populates="url")
+    clicks_logs: Mapped[list["ClickLog"]] = relationship(
+        "ClickLog", back_populates="url"
+    )
 
 
 class ClickLog(Base):
     __tablename__ = "click_logs"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    url_id: Mapped[int] = mapped_column(ForeignKey("urls.id"), nullable=False, index=True)
-    clicked_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), index=True)
+    url_id: Mapped[int] = mapped_column(
+        ForeignKey("urls.id"), nullable=False, index=True
+    )
+    clicked_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), index=True
+    )
 
     url: Mapped["URL"] = relationship("URL", back_populates="clicks_logs")
